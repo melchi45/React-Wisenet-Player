bde79d671a69f00307ad540626cd3a3fcab5bc56
Youngho Kim <melchi45@gmail.com>
2022-06-15 14:28:09 +0900

    copy favicon to root directory

--- webpack.config.js
+++ webpack.config.js
@@ -138,6 +138,15 @@ var options = {
     new CopyWebpackPlugin({
       patterns: [
         {
+          from: 'src/assets/icons/favicon.ico',
+          to: path.join(__dirname, 'build'),
+          force: true,
+        },
+      ],
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
           from: 'src/pages/Content/content.styles.css',
           to: path.join(__dirname, 'build'),
           force: true,
